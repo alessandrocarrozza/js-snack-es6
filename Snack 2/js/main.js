bbc4cff4a45fd0a6aaf5d115bbae19045864dc49
Alessandro Carrozza <alessandrowcarrozza@gmail.com>
2023-03-10 18:48:10 +0100

    Secondo array creato - snack completato

--- Snack 2/js/main.js
+++ Snack 2/js/main.js
@@ -3,17 +3,42 @@
 
 const soccerTeams = [
     {
-        name : "napoli",
+        teamName : "napoli",
         "punti fatti" : 0,
         "falli subiti" : 0,
     },
     {
-        name : "juventus",
+        teamName : "juventus",
         "punti fatti" : 0,
         "falli subiti" : 0,
     },
     {
-        name : "cremonese",
+        teamName : "cremonese",
+        "punti fatti" : 0,
+        "falli subiti" : 0,
+    },
+    {
+        teamName : "atalanta",
+        "punti fatti" : 0,
+        "falli subiti" : 0,
+    },
+    {
+        teamName : "benevento",
+        "punti fatti" : 0,
+        "falli subiti" : 0,
+    },
+    {
+        teamName : "torino",
+        "punti fatti" : 0,
+        "falli subiti" : 0,
+    },
+    {
+        teamName : "roma",
+        "punti fatti" : 0,
+        "falli subiti" : 0,
+    },
+    {
+        teamName : "lazio",
         "punti fatti" : 0,
         "falli subiti" : 0,
     }
@@ -25,14 +50,22 @@ console.log(soccerTeams);
 
 for (let i = 0; i < soccerTeams.length; i++) {
 
-    soccerTeams[i]["punti fatti"] = generateRandomNumber(1, 30);
+    soccerTeams[i]["punti fatti"] = generateRandomNumber(1, 80);
     soccerTeams[i]["falli subiti"] = generateRandomNumber(1, 30);
 }
 
-console.log(soccerTeams)
+
+
+const teamNomeFalli = soccerTeams.map(({teamName, ["falli subiti"]:falliSubiti}) => {
+    return {teamName, falliSubiti};
+});
+
+console.log(teamNomeFalli);
 
 
 function generateRandomNumber (min, max) {
     const randomNumber = Math.floor(Math.random() * max) + min;
     return randomNumber;
 }
+
+
