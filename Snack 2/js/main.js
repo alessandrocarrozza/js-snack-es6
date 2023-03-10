c2cf82cd2458c53b7aabaa0caeac04ca3641b7df
Alessandro Carrozza <alessandrowcarrozza@gmail.com>
2023-03-10 17:42:36 +0100

    Funzione randomNumber creata

--- Snack 2/js/main.js
+++ Snack 2/js/main.js
@@ -1,4 +1,32 @@
 
 // ------ SECONDO SNACK -------
 
+const soccerTeams = [
+    {
+        name : "napoli",
+        "punti fatti" : 0,
+        "falli subiti" : 0,
+    },
+    {
+        name : "juventus",
+        "punti fatti" : 0,
+        "falli subiti" : 0,
+    },
+    {
+        name : "cremonese",
+        "punti fatti" : 0,
+        "falli subiti" : 0,
+    }
+];
 
+console.log(soccerTeams);
+
+
+function generateRandomNumber (min, max) {
+    const randomNumber = Math.floor(Math.random() * max) + min;
+    return randomNumber;
+}
+
+const casualNumber = generateRandomNumber(1, 30)
+
+console.log(casualNumber);
