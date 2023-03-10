691058646a73af81d4a9cf3f5fc184bb6453bafe
Alessandro Carrozza <alessandrowcarrozza@gmail.com>
2023-03-10 18:16:52 +0100

    Valori casuali negli oggetti

--- Snack 2/js/main.js
+++ Snack 2/js/main.js
@@ -22,11 +22,17 @@ const soccerTeams = [
 console.log(soccerTeams);
 
 
+
+for (let i = 0; i < soccerTeams.length; i++) {
+
+    soccerTeams[i]["punti fatti"] = generateRandomNumber(1, 30);
+    soccerTeams[i]["falli subiti"] = generateRandomNumber(1, 30);
+}
+
+console.log(soccerTeams)
+
+
 function generateRandomNumber (min, max) {
     const randomNumber = Math.floor(Math.random() * max) + min;
     return randomNumber;
 }
-
-const casualNumber = generateRandomNumber(1, 30)
-
-console.log(casualNumber);
